9e43fc454c38525a53cba6ad07f17381a421778e
Surya Raman <ayrusme@gmail.com>
2016-08-09 00:01:44 +0530

    Added problem two

---
+++ String_Manipulation/String_Manipulation.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+#include<string>
+using namespace std;
+
+void main()
+{   //Declaring a character array with a maximum size of 30 characters
+	char s[30];
+	
+	//Visual Studio wouldn't let me use gets, so utilising the more safe gets_s
+	gets_s(s);
+
+	//Calculating the length of string so that loop doesn't go infinite
+	int len = strlen(s);
+
+	//Adding each character by one, it'll increment the character's ASCII value. The mod 26 will keep it from 
+	//going out of bounds.
+	for (int i = 0; i < len; i++)
+	{
+		if (s[i] != ' ')
+		s[i] = ( s[i] + 1) ;
+	}
+
+	//Printing the result.
+	cout << "\n";
+	puts(s);
+
+	_getch();
+
+}
