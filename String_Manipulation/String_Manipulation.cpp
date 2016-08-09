4468fe00061d92a1cb8fc85f4885d780f5e96026
Surya Raman <ayrusme@gmail.com>
2016-08-09 23:50:32 +0530

    Changed code to reflect the problem statement

--- String_Manipulation/String_Manipulation.cpp
+++ String_Manipulation/String_Manipulation.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 #include<conio.h>
-#include<string>
+#include<string.h>
 using namespace std;
 
 void main()
 {   //Declaring a character array with a maximum size of 30 characters
-	char s[30];
+	char s[30], cip[30];
 
 	//Visual Studio wouldn't let me use gets, so utilising the more safe gets_s
 	gets_s(s);
@@ -13,23 +13,37 @@ void main()
 	//Calculating the length of string so that loop doesn't go infinite
 	int len = strlen(s);
 
-	//Adding each character by one, it'll increment the character's ASCII value. The initial condition will keep it from 
-	//going out of bounds.
+	/*The difference between a lower case alphabet and the next alphabet in uppercase is always 31.
+	Using this, we can set the cipher to the respective alphabet. For consecutive alphabets, the cipher 
+	incremented accordingly*/
+
 	for (int i = 0; i < len; i++)
 	{
-		if (s[i] == 'z')
+		if (s[i] != 'z')
 		{
-			s[i] = 'a';
+
+			if (s[i] != ' ')
+			{
+				cip[i] = s[i] - 31;
+			}
+			if (cip[i] == cip[i - 1])
+			{
+				cip[i] = cip[i] + 1;
+			}
 		}
-		else if (s[i] != ' ')
+		else
 		{
-			s[i] = (s[i] + 1);
+			cip[i] = 'A';
+			if (cip[i] == cip[i - 1])
+			{
+				cip[i] = cip[i] + 1;
+			}
 		}
 	}
 
 	//Printing the result.
 	cout << "\n";
-	puts(s);
+	puts(cip);
 
 	_getch();
 
